bb8e0457d55217f684108ba614908794033bf243
Ry Mercado <rytmercado@gmail.com>
2022-03-28 12:03:43 -0600

    added instruction screen

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import Game from './scripts/game';
 import GameView from './scripts/game_view';
 
 window.addEventListener('DOMContentLoaded', function() {
+    const instruction = document.getElementById('instruction-canvas')
+    instruction.addEventListener('click', function(){
+        instruction.remove();
+    })
     const canvas = {
         mapCanvas: document.getElementById('map-canvas'),
         gameCanvas: document.getElementById("game-canvas"),
     }
+    
     // const context = canvas.getContext('2d');
     const game = new Game();
     const background = new GameView(canvas, game);
